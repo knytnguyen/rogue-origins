fa468678ce78e5cf4e9f8d75b7ad61d2b5a80a92
knytnguyen <knytnguyen@gmail.com>
2022-02-24 01:05:38 -0800

    Created functioning main menu features with the Character class

---
+++ header/character.hpp
@@ -0,0 +1,42 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include<string>
+
+using namespace std;
+
+class Character {
+private:
+    /* data */
+    string name;
+    int characterLevel;
+    int experiencePoints;
+    int experienceNext;
+    int healthPoints;
+    int attackPower;
+    int coins;
+
+public:
+    //constructors:
+    Character();
+    
+    //destructors:
+    ~Character();
+
+    //functions:
+    void createInitialCharacter(string name);
+    void initializeEasy(string name); // depending on the difficulty, we are going to initilize the character;
+    void initializeMedium(string name); // depending on the difficulty, we are going to initilize the character;
+    void initializeHard(string name); // depending on the difficulty, we are going to initilize the character;
+
+    //accessors:
+    inline const string& getName() const { return this->name; }
+    inline const int& getLevel() const { return this->characterLevel; }
+    inline const int& getExperiencePoints() const { return this->experiencePoints; }
+    inline const int& getExperienceNext() const { return this->experienceNext; }
+    inline const int& getHealthPoints() const { return this->healthPoints; }
+    inline const int& getAttackPower() const { return this->attackPower; }
+    inline const int& getCoins() const { return this->coins; }
+};
+
+#endif
