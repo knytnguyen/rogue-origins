37fa9b0c9f1cede09245d9e638174804fa67cb70
knytnguyen <knytnguyen@gmail.com>
2022-03-09 22:56:35 -0800

    Created additional working unit tests for the Player class

--- tests/player_test.hpp
+++ tests/player_test.hpp
@@ -10,6 +10,12 @@
 
 using namespace std;
 
+TEST(Player_Test, PlayerConstructorTest) {
+	Player userPlayer;
+	string playerNameTest = userPlayer.getName();
+	EXPECT_EQ("NONE", playerNameTest);
+}
+
 TEST(Player_Test, GetPlayerName) {
 	string playerName = "Valor";
 	vector<Player> playableCharacter;
@@ -20,6 +26,42 @@ TEST(Player_Test, GetPlayerName) {
 
 	string playerNameTest = playableCharacter[currentPlayer].getName();
 	EXPECT_EQ("Valor", playerNameTest);
+}
+
+TEST(Player_Test, GetPlayerHealthEasyDifficulty) {
+	string playerName = "Valor";
+        vector<Player> playableCharacter;
+        int currentPlayer;
+        playableCharacter.push_back(Player());
+        currentPlayer = playableCharacter.size() - 1;
+        playableCharacter[currentPlayer].initializeEasy(playerName);
+
+	int playerHealthTest = playableCharacter[currentPlayer].getHealthPoints();
+	EXPECT_EQ(100, playerHealthTest);
+}
+
+TEST(Player_Test, GetPlayerAttackMediumDifficulty) {
+        string playerName = "Valor";
+        vector<Player> playableCharacter;
+        int currentPlayer;
+        playableCharacter.push_back(Player());
+        currentPlayer = playableCharacter.size() - 1;
+        playableCharacter[currentPlayer].initializeMedium(playerName);
+
+	int playerAttackTest = playableCharacter[currentPlayer].getAttackPower();
+	EXPECT_EQ(20, playerAttackTest);
+}
+
+TEST(Player_Test, GetPlayerDefenseHardDifficulty) {
+        string playerName = "Valor";
+        vector<Player> playableCharacter;
+        int currentPlayer;
+        playableCharacter.push_back(Player());
+        currentPlayer = playableCharacter.size() - 1;
+        playableCharacter[currentPlayer].initializeHard(playerName);
+
+	int playerDefenseTest = playableCharacter[currentPlayer].getDefense();
+	EXPECT_EQ(5, playerDefenseTest);
 }
 
 #endif
