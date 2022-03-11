49676871e4a10ffcc21ed93d6807b262acb5d32f
alvin-vo <av.alvinvo@gmail.com>
2022-03-11 00:42:11 -0800

    Create enemy header

---
+++ header/Enemy.hpp
@@ -0,0 +1,22 @@
+#ifndef __ENEMY_HPP__
+#define __ENEMY_HPP__
+
+#include "../header/character.hpp"
+
+#include <string>
+
+class Enemy: public Character {
+    /* Do we even need this? */
+};
+
+class Archmage: public Enemy {
+    public:
+        Archmage() {this->healthPoints = 200; this->attackPower = 15;}
+        ~Archmage();
+    
+        void attack(Character* p) {
+            p->healthChange(this->getAttack());
+        }
+};
+
+#endif
