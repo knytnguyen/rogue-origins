9bae9c0ff753930e26cb6f9591585d6a193c3920
ehsu092 <97259950+ehsu092@users.noreply.github.com>
2022-03-11 01:25:59 -0800

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "shop_test.hpp"
 
-#include "player_test.hpp"
+//#include "player_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
